ecf9003bf67efae9f881452aa80f67788178f4cc
Manha Cetcc <Manha.Cetcc@labfies.org.br>
2026-05-12 10:53:21 -0300

    oi

--- clima.js
+++ clima.js
@@ -1,11 +1,15 @@
 async function clima() {
     const cidade = document.getElementById('cidade').value
 
-    const response = await fetch(``)
     const chave = "194448f2d8536e82d4549d611f1300fa"
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=${chave}`)
+    
 
     const dados = await response.json()
-    document.getElementById('bandeira').src = dados[0].flags.png
-    document.getElementById('capital').innerHTML = dados[0].capital
-    document.getElementById('populacao').innerHTML = dados[0].population
+    document.getElementById('temperatura').innerHTML = dados.main.temp
+    document.getElementById('tempMax').innerHTML = dados.main.temp_max
+    document.getElementById('tempMin').innerHTML = dados.main.temp_min
+    document.getElementById('vento').innerHTML = dados.wind.speed
+    document.getElementById('umidade').innerHTML = dados.main.humidity
+    
 }
